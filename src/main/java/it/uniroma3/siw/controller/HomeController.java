dfd322d10ba0daae3d4a16ff3a9c30308118a87c
jbest152 <bjunzharold@yahoo.com>
2025-07-02 13:50:41 +0200

    Refactor home.html and HomeController

--- src/main/java/it/uniroma3/siw/controller/HomeController.java
+++ src/main/java/it/uniroma3/siw/controller/HomeController.java
@@ -7,16 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.model.User;
 import it.uniroma3.siw.service.AuthorService;
 import it.uniroma3.siw.service.BookService;
+import it.uniroma3.siw.service.CredentialsService;
+
+import static it.uniroma3.siw.model.Credentials.ADMIN_ROLE;
 
 @Controller
-public class HomepageController {
+public class HomeController {
 
 	@Autowired
 	private AuthorService authorService;
+	
 	@Autowired
 	private BookService bookService;
+	
+	@Autowired
+	private CredentialsService credentialsService;
 
 	@GetMapping("admin/bookIndex")
 	public String homeBook(Model model) {
@@ -30,13 +39,19 @@ public class HomepageController {
 	
 	@GetMapping("/") 
     public String home(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+		User user = null;
+		boolean isAdmin = false;
+		if (userDetails != null) {
+			Credentials c = credentialsService.getCredentials(userDetails.getUsername());
+			user = c.getUser();
+			isAdmin = c.getRole().equals(ADMIN_ROLE);
+		}
+		model.addAttribute("isAdmin", isAdmin);
+		model.addAttribute("user", user);
 		model.addAttribute("bookCount", bookService.countBooks());
 		model.addAttribute("authorCount", authorService.countAuthors());
 		model.addAttribute("latestBooks", bookService.findLatestBooks(5));
-		if (userDetails == null)
-			return "homepage";
-		model.addAttribute("username", userDetails.getUsername());
-        return "homepage";
+        return "home";
     }
 	
 	@GetMapping("/home")
